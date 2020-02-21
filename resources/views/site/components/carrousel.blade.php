a11c11ef7ca9793a4bf4dee3607666f211e5e495
Tauan Almeida <tauan_almeida@hotmail.com>
2020-02-21 17:17:39 -0300

    Modificação para adpatação ao laravel

---
+++ resources/views/site/components/carrousel.blade.php
@@ -0,0 +1,66 @@
+<div class="banner">
+        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+            </ol>
+            <div class="carousel-inner" role="listbox">
+                <div class="carousel-item active">
+                    <div class="carousel-caption">
+                        <h3>Somos mais do que uma empresa industrial</h3>
+
+                        <div class="bnr-button">
+                            <button type="button" class="btn btn-primary play" data-toggle="modal" data-target="#exampleModal">
+								<i class="fas fa-play"></i>
+							</button>
+                        </div>
+
+                    </div>
+                </div>
+                <div class="carousel-item item2">
+                    <div class="carousel-caption">
+                        <h3>Transformando grandes idéias em ótimos produtos</h3>
+
+                        <div class="bnr-button">
+                            <button type="button" class="btn btn-primary play" data-toggle="modal" data-target="#exampleModal">
+								<i class="fas fa-play"></i>
+							</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="carousel-item item3">
+                    <div class="carousel-caption">
+                        <h3>Sua sastifação é a nossa motivação</h3>
+
+                        <div class="bnr-button">
+                            <button type="button" class="btn btn-primary play" data-toggle="modal" data-target="#exampleModal">
+								<i class="fas fa-play"></i>
+							</button>
+                        </div>
+
+                    </div>
+                </div>
+                <div class="carousel-item item4">
+                    <div class="carousel-caption">
+                        <h3>Transformando sonhos em realidade</h3>
+
+                        <div class="bnr-button">
+                            <button type="button" class="btn btn-primary play" data-toggle="modal" data-target="#exampleModal">
+								<i class="fas fa-play"></i>
+							</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+				<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+				<span class="sr-only">Previous</span>
+			</a>
+            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+				<span class="carousel-control-next-icon" aria-hidden="true"></span>
+				<span class="sr-only">Next</span>
+			</a>
+        </div>
+    </div>
